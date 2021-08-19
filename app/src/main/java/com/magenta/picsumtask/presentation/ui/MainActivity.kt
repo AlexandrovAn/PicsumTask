ebb64065ac5c3f95ba1c9799a5fac252a67d9a8a
AlexandrovAn <57633337+AlexandrovAn@users.noreply.github.com>
2021-08-19 17:59:45 +0400

    Initial commit

---
+++ app/src/main/java/com/magenta/picsumtask/presentation/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.magenta.picsumtask.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.magenta.picstask.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
