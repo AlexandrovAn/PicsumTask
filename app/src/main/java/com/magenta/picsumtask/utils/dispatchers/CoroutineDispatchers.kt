2245e05b29ddaa9cbacb88013d79726efb1644e4
AlexandrovAn <57633337+AlexandrovAn@users.noreply.github.com>
2021-08-27 00:19:46 +0400

    add viewmodel test

--- app/src/main/java/com/magenta/picsumtask/utils/dispatchers/CoroutineDispatchers.kt
+++
@@ -1,9 +0,0 @@
-package com.magenta.picsumtask.utils.dispatchers
-
-import kotlinx.coroutines.CoroutineDispatcher
-
-interface CoroutineDispatchers {
-    val default: CoroutineDispatcher
-    val io: CoroutineDispatcher
-    val main: CoroutineDispatcher
-}
